237bd8c93889657d990c5190f1636cdb1a07e09c
lelle1000 <linusestrandberg@gmail.com>
2026-03-31 13:10:25 +0200

    Added cors

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ import { serveFile, serveDir } from "jsr:@std/http/file-server";
 
 function handler() {
 
+	let url = new URL(request.url)
+
+	const headerCORS = new Headers()
+
+	headerCORS.set("Access-Control-Allow-Origin", "*");
+	headerCORS.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+	headerCORS.set("Access-Control-Allow-Headers", "Content-Type");
+	if (request.method == "OPTIONS") {
+		return new Response(null, { headers: headerCORS })
+	}
+
+
+
     return serveDir(request, {
 		fsRoot: "/",
 		urlRoot: "",
 		showDirListing: false
 	});
+
+
 }
 
 Deno.serve(handler);
