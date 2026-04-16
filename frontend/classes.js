20466436adc5d9b38000e4ad26c928203a80215e
lelle1000 <linusestrandberg@gmail.com>
2026-04-16 14:15:27 +0200

    fffdf

--- frontend/classes.js
+++ frontend/classes.js
@@ -1,27 +1,18 @@
  let pageArray = [];
  let chapters = document.querySelectorAll(".chapter") 
-for(let i = 1; i < chapters.length; i++) {
+for(let i = 1; i <= chapters.length; i++) {
     pageArray.push(document.querySelector(`#chapter${i}`))
 }
 
-console.log(pageArray);
-
  
 
-class pubSub {
-
-    static activeSubscription = [];
+class showPage {
 
     constructor(item){
         item = this.item
-
-        activeSubscription.push(item)
     }
 
-
-
-
-    activePage(page) {
+    activePage(pageArray) {
         forEach(page => page.classList.add("hide"))
     }
 
