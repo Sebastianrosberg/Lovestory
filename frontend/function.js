31e77537698b445a1533733156c7ff2e6970e9b7
yehorgorstein <gorstein.yehor@gmail.com>
2026-04-28 09:42:07 +0200

    wooo

--- frontend/function.js
+++ frontend/function.js
@@ -1,11 +1,9 @@
 function switchPage(page) {
     allPages.forEach(e => {
         e.classList.add("hide")
-        e.classList.remove("reveal")
     })
     let myPage = document.querySelector(`#${page}`)
     myPage.classList.remove("hide")
-    myPage.classList.add("reveal")
 }
 
 
