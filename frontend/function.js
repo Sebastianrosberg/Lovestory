9b65bd83c58d7c2ea151229e5c5670318b6ae25d
lelle1000 <linusestrandberg@gmail.com>
2026-05-07 14:45:42 +0200

    fixed startPage

--- frontend/function.js
+++ frontend/function.js
@@ -1,8 +1,8 @@
 function switchPage(page) {
     allPages.forEach(e => {
         e.classList.add("hide")
+        e.classList.remove("reveal")
     })
-    let myPage = document.querySelector(`#${page}`)
-    myPage.classList.remove("hide")
-    myPage.classList.add("reveal")
+    page.classList.remove("hide")
+    page.classList.add("reveal")
 }
